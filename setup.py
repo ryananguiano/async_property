dd65f185959d2b02255ccb9644245c1b80f5f60b
Ryan Anguiano <ryan.anguiano@gmail.com>
2019-04-12 01:49:52 -0400

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ryananguiano/async_property',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
